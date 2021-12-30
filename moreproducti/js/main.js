3b74a247d09474c4241b2224170fad7bb9e2bf08
Pavel <pavelefimov2203@gmail.com>
2021-12-30 18:50:49 +0300

    po

---
+++ moreproducti/js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+
+  $('.header-slider').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../img/header/prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../img/header/next.svg" alt=""></button>',
+    fade: true
+  });
+
+});
